c5bc59bb7a23820ccf1a8e0772a2b2b25bcceb18
HaeRyeong Jin <jhr5559@gmail.com>
2025-09-28 21:49:15 +0900

    Style: admin 대시보드와 default 페이지들의 레이아웃 분리

---
+++ client/src/pages/Member.tsx
@@ -0,0 +1,11 @@
+import MemberList from "../components/member/MemberList";
+import '../assets/styles/member.css';
+const Member = () => {
+    return (
+        <div>
+            <MemberList />
+        </div>
+    );
+}
+
+export default Member;
